a8c3df1ad8059aa143e77b32f1496f542feef874
Suresh Kumar Putchala <sputchala@seekasia.com>
2023-04-06 22:04:48 +0530

    added prompt for env generation confirmation

--- bin/setupEnv.sh
+++ bin/setupEnv.sh
@@ -2,6 +2,14 @@
 
 touch .env
 
+read -p "Do you wish to generate env? : (y/n) " prompt
+
+if [[ "${prompt}" != "y" ]]
+then
+  exit 0
+fi
+
+
 echo 'PORT="4000"' > .env
 echo 'API_DOMAIN="https://api.napster.com/v2.2"' >> .env
 echo 'IMAGE_DOMAIN="https://api.napster.com/imageserver/v2"' >> .env
